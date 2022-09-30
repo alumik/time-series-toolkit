eda6e896a4232a8a809b5bdb738c0030897bbf71
Zhong Zhenyu <nczzy1997@gmail.com>
2022-09-30 12:20:20 +0800

    Make the plotting function more customizable

--- tskit/plot.py
+++ tskit/plot.py
@@ -5,7 +5,15 @@ import tskit
 from typing import *
 
 
-def plot(ts: tskit.TimeSeries, figsize: tuple[int] = (12, 2), title: Optional[str] = None):
+def plot(
+        ts: tskit.TimeSeries,
+        ax: Optional[plt.Axes] = None,
+        tight_layout: bool = True,
+        show: bool = True,
+        figsize: tuple[int] = (12, 2),
+        title: Optional[str] = None,
+        **kwargs,
+):
     """
     Plot a TimeSeries.
 
@@ -15,21 +23,45 @@ def plot(ts: tskit.TimeSeries, figsize: tuple[int] = (12, 2), title: Optional[st
     ----------
     ts: tskit.TimeSeries
         The TimeSeries to plot.
+    ax: matplotlib.Axes, optional, default: None
+        The axes to plot on. If None, a new figure will be created.
+    tight_layout: bool, optional, default: True
+        Whether to use tight layout.
+    show: bool, optional, default: True
+        Whether to show the plot.
     figsize: tuple, optional, default: (12, 2)
         The size of the figure.
     title: str, optional, default: None
         The title of the plot.
+
+    Other Parameters
+    ----------------
+    **kwargs
+        Additional keyword arguments to pass to the plotting function.
     """
     if ts.values.ndim == 1:
-        _plot_univariate_time_series(ts, figsize=figsize, title=title)
+        plotting_func = _plot_univariate_time_series
     else:
-        raise NotImplementedError('Only univariate time series are supported.')
+        raise NotImplementedError('Only univariate time series are supported at the moment.')
+    plotting_func(
+        ts,
+        ax=ax,
+        tight_layout=tight_layout,
+        show=show,
+        figsize=figsize,
+        title=title,
+        **kwargs,
+    )
 
 
 def _plot_univariate_time_series(
         ts: tskit.TimeSeries,
+        ax: Optional[plt.Axes] = None,
+        tight_layout: bool = True,
+        show: bool = True,
         figsize: tuple[int] = (12, 2),
         title: Optional[str] = None,
+        **kwargs,
 ):
     """
     Plot a univariate TimeSeries.
@@ -38,17 +70,33 @@ def _plot_univariate_time_series(
     ----------
     ts: tskit.TimeSeries
         The TimeSeries to plot.
+    ax: matplotlib.Axes, optional, default: None
+        The axes to plot on. If None, a new figure will be created.
+    tight_layout: bool, optional, default: True
+        Whether to use tight layout.
+    show: bool, optional, default: True
+        Whether to show the plot.
     figsize: tuple, optional, default: (12, 2)
         The size of the figure.
     title: str, optional, default: None
         The title of the plot.
+
+    Other Parameters
+    ----------------
+    **kwargs
+        Additional keyword arguments to pass to the plotting function.
     """
-    fig = plt.figure(figsize=figsize)
-    ax = fig.subplots()
-    ax.plot(ts.index, ts.values)
-    title = ts.name if title is None else title
-    ax.set_title(title)
+    if ax is None:
+        fig, ax = plt.subplots(figsize=figsize)
+    else:
+        fig = ax.get_figure()
+
+    ax.plot(ts.index, ts.values, **kwargs)
+    ax.set_title(ts.name if title is None else title)
+
     if ts.index.dtype == 'datetime64[ns]':
         fig.autofmt_xdate()
-    fig.tight_layout()
-    fig.show()
+    if tight_layout:
+        fig.tight_layout()
+    if show:
+        fig.show()
